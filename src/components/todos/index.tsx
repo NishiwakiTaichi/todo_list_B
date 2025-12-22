b9802dc61f63b81d051998a56a1cc84f84726d22
Nishiwaki <plusonefor730@gmail.com>
2025-12-23 03:28:07 +0900

    Finish feature/complated_delete_flg(ReactでTodoアプリを作ろう_3完了)

--- src/components/todos/index.tsx
+++ src/components/todos/index.tsx
@@ -2,61 +2,98 @@ import React, { useState } from 'react';
 
 // "Todo" 型の定義をコンポーネント外で行います
 type Todo = {
-  id: number; //★
   title: string;
   readonly id: number;
+  completed_flg: boolean;
+  delete_flg: boolean, // <-- 追加
 };
 
 
 // Todo コンポーネントの定義
-const Todos: React.FC = () => { //★
-const [todos, setTodos] = useState<Todo[]>([]); // Todo配列として初期化
-  const [text, setText] = useState(''); // テキスト入力用
+const Todo: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]); // Todoの配列を保持するステート
+  const [text, setText] = useState(''); // フォーム入力のためのステート
   const [nextId, setNextId] = useState(1); // 次のTodoのIDを保持するステート
-  
+
   // todos ステートを更新する関数
   const handleSubmit = () => {
     // 何も入力されていなかったらリターン
     if (!text) return;
-  
-  
-  
+
     // 新しい Todo を作成
     const newTodo: Todo = {
-      title: text, // text ステートの値を content プロパティへ
+      title: text, // text ステートの値を title プロパティへ
       id: nextId,
+      // 初期値は false
+      completed_flg: false,
+      delete_flg: false, // <-- 追加
     };
 
-
+    /**
+     * 更新前の todos ステートを元に
+     * スプレッド構文で展開した要素へ
+     * newTodo を加えた新しい配列でステートを更新
+     **/
     setTodos((prevTodos) => [newTodo, ...prevTodos]);
     setNextId(nextId + 1); // 次の ID を更新
-  
-  
+
+
     // フォームへの入力をクリアする
     setText('');
   };
 
-const handleEdit = (id: number, value: string) => {
-  setTodos((todos) => {
-    const newTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        // 新しいオブジェクトを作成して返す
-        return { ...todo, title: value };
-      }
-      return todo;
+
+  const handleEdit = (id: number, value: string) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          // 新しいオブジェクトを作成して返す
+          return { ...todo, title: value };
+        }
+        return todo;
+      });
+  
+      // todos ステートが書き換えられていないかチェック
+      console.log('=== Original todos ===');
+      todos.map((todo) => {
+        console.log(`id: ${todo.id}, title: ${todo.title}`);
+      });
+  
+  
+      return newTodos;
     });
+  };
 
 
-    // todos ステートが書き換えられていないかチェック
-    console.log('=== Original todos ===');
-    todos.map((todo) => {
-      console.log(`id: ${todo.id}, title: ${todo.title}`);
+  const handleCheck = (id: number, completed_flg: boolean) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed_flg };
+        }
+        return todo;
+      });
+
+
+      return newTodos;
     });
+  };
 
 
-    return newTodos;
-  });
-};
+  const handleRemove = (id: number, delete_flg: boolean) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, delete_flg };
+        }
+        return todo;
+      });
+
+
+      return newTodos;
+    });
+  };
+
 
   return (
     <div>
@@ -70,25 +107,36 @@ const handleEdit = (id: number, value: string) => {
           type="text"
           value={text} // フォームの入力値をステートにバインド
           onChange={(e) => setText(e.target.value)} // 入力値が変わった時にステートを更新
-          placeholder="タスクを入力してください"  //★
         />
-         <button className="insert-btn" type="submit">追加</button>{/* ボタンをクリックしてもonSubmitをトリガーしない */}
+        <button className="insert-btn" type="submit">追加</button> {/* ボタンをクリックしてもonSubmitをトリガーしない */}
       </form>
       <ul>
         {todos.map((todo) => {
           return (
             <li key={todo.id}>
               <input
+                type="checkbox"
+                checked={todo.completed_flg}
+                // 呼び出し側で checked フラグを反転させる
+                onChange={() => handleCheck(todo.id, !todo.completed_flg)}
+              />
+              <input
                 type="text"
                 value={todo.title}
+                disabled={todo.completed_flg}
                 onChange={(e) => handleEdit(todo.id, e.target.value)}
               />
+              <button onClick={() => handleRemove(todo.id, !todo.delete_flg)}>
+                {todo.delete_flg ? '復元' : '削除'}
+              </button>
             </li>
-            );
-          })}
-        </ul>
+          );
+        })}
+      </ul>
     </div>
   );
 };
 
-export default Todos;
+
+
+export default Todo;
